f98d2bc16a3f7462002029895637d1f5765dc392
dudu <dudubarra7@gmail.com>
2025-09-05 17:21:07 -0300

    Primeiro commit

---
+++ src/main/java/com/dudly/cadastro_usuario/controller/UsuarioController.java
@@ -0,0 +1,32 @@
+package com.dudly.cadastro_usuario.controller;
+
+import com.dudly.cadastro_usuario.business.UsuarioService;
+import com.dudly.cadastro_usuario.infrastructure.entitys.Usuario;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/usuario")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<Void> salvarUsuario(@RequestBody Usuario usuario){
+        usuarioService.salvarUsuario(usuario);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Usuario> buscarUsuarioPorEmail(@RequestParam String email){
+        return ResponseEntity.ok(usuarioService.buscarUsuarioPorEmail(email));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deletarUsuarioPorEmail(@RequestParam String email){
+        return ResponseEntity.ok().build();
+    }
+}
